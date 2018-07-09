739b79717fe54c0767617cf86934e85577a79fa5
Zac Willmington <zac.willmington@gmail.com>
2018-07-09 23:36:33 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180709233106) do
+ActiveRecord::Schema.define(version: 20180709233610) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180709233106) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
